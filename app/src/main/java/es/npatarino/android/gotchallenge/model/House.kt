4c8784e5444fec03927a41830f0173e699e50989
Alfonso Cuitiño <alfonso.cuiti@gmail.com>
2019-11-12 09:48:23 -0300

    Implement equals and hashcode to avoid having duplicated houses

--- app/src/main/java/es/npatarino/android/gotchallenge/model/House.kt
+++ app/src/main/java/es/npatarino/android/gotchallenge/model/House.kt
@@ -5,4 +5,8 @@ import java.io.Serializable
 
 data class House(@SerializedName("houseImageUrl") val imageUrl: String,
                  @SerializedName("houseName") val name: String,
-                 @SerializedName("houseId") val id: String) : Serializable
+                 @SerializedName("houseId") val id: String) : Serializable {
+
+    override fun equals(other: Any?) = (other is House) && other.id == id
+    override fun hashCode() = id.hashCode()
+}
